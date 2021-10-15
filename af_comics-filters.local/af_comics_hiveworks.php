b74655782ed3ba10d17699872a69c9218dbc9052
Mark Furland <mtfurlan@i3detroit.org>
2021-10-15 11:43:42 -0400

    fix hiveworks undefined variables

--- af_comics-filters.local/af_comics_hiveworks.php
+++ af_comics-filters.local/af_comics_hiveworks.php
@@ -5,6 +5,14 @@ class Af_Comics_Hiveworks extends Af_ComicFilter {
 		return array("Many hiveworks RSS feeds, with special stuff for SMBC");
 	}
 
+	function getFirstXpathQueryResultOrNull($xpath, $query) {
+		$results = $xpath->query($query);
+		if($results->length == 0) {
+			return null;
+		}
+		return $results->item(0);
+	}
+
 	function process(&$article) {
 		if (strpos($article["author"], "tech@thehiveworks.com") === FALSE) {
 			return false;
@@ -24,13 +32,12 @@ class Af_Comics_Hiveworks extends Af_ComicFilter {
 			$mouseover = $comicNode->getAttribute('title');
 			//$imgURL = $comicNode->getAttribute('src');
 
-			$newsHeader = $xpath->query('//div[@class="cc-newsheader"]')->item(0)->textContent;
-			$publishTime = $xpath->query('//div[@class="cc-publishtime"]')->item(0)->textContent;
-			$newsContentNode = $xpath->query('//div[@class="cc-newsbody"]')->item(0);
+			$newsHeader = $this->getFirstXpathQueryResultOrNull($xpath, '//div[@class="cc-newsheader"]');
+			$publishTime = $this->getFirstXpathQueryResultOrNull($xpath, '//div[@class="cc-publishtime"]');
+			$newsContentNode = $this->getFirstXpathQueryResultOrNull($xpath, '//div[@class="cc-newsbody"]');
 
-			$hasNews = $newsContentNode;
+			//Debug::log(print_r($newsContentNode, TRUE), Debug::$LOG_EXTENDED);
 
-			Debug::log(print_r($newsContentNode, TRUE), Debug::$LOG_EXTENDED);
 			if (strpos($article["link"], "smbc-comics.com/comic") !== FALSE) {
 				$extraContent = $xpath->query('(//div[@id="aftercomic"][1])')->item(0)->firstChild;
 			}
@@ -43,16 +50,18 @@ class Af_Comics_Hiveworks extends Af_ComicFilter {
 				if($mouseover) {
 					$article["content"] .= "<br><span>$mouseover</span><br>";
 				}
-				if($extraContent) {
+				if(isset($extraContent)) {
 					$article["content"] .= $doc->saveHTML($extraContent);
 				}
-				if($hasNews) {
-					if($newsHeader) {
-						$article["content"] .= "<h2>$newsHeader</h2>";
+				if($newsContentNode) {
+					if(isset($newsHeader)) {
+						$article["content"] .= "<h2>$newsHeader->textContent</h2>";
 					} else {
 						$article["content"] .= "<h2>News</h2>";
 					}
-					$article["content"] .= "<small>$publishTime</small>";
+					if(isset($publishTime)) {
+						$article["content"] .= "<small>$publishTime->textContent</small>";
+					}
 					$article["content"] .= $doc->saveHTML($newsContentNode);
 				}
 				$article["content"] .= "</div>";
